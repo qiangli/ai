ee265623c8cc52135a6f35d5a9efda749dac24fd
Qiang Li <liqiang@gmail.com>
2025-10-20 13:55:04 -0700

    update model config in agent

---
+++ swarm/atm/conf/agent_test.go
@@ -0,0 +1,31 @@
+package conf
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func TestLoadAgentData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	file, err := os.Open("../resource/incubator/agents/gptr/agent.yaml")
+	if err != nil {
+		t.Fatalf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	d, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatalf("failed to read file: %v", err)
+	}
+
+	ac, err := LoadAgentsData([][]byte{d})
+	if err != nil {
+		t.Fatalf("failed to load file: %v", err)
+	}
+	for _, v := range ac.Agents {
+		t.Logf("%+v", v)
+	}
+}
