5bd49319a750480e6c2b4fd86531b62851741e83
Qiang Li <liqiang@gmail.com>
2025-12-09 18:48:02 -0800

    Remove dead code

--- cmd/main.go
+++ cmd/main.go
@@ -9,38 +9,6 @@ import (
 	"github.com/qiangli/ai/internal/agent"
 )
 
-// const rootUsageTemplate = `AI Command Line Tool
-
-// Usage:
-//   ai [OPTIONS] [@AGENT] MESSAGE...{{if .HasExample}}
-
-// Examples:
-// {{.Example}}{{end}}
-
-// Use "{{.CommandPath}} /help [agents|tools|models]" for more information.
-// `
-
-// const usageExample = `
-// ai what is fish
-// ai @ask what is fish
-// `
-
-// var rootCmd = &cobra.Command{
-// 	Use:                   "ai [OPTIONS] [@AGENT] MESSAGE...",
-// 	Short:                 "AI Command Line Tool",
-// 	Example:               usageExample,
-// 	DisableFlagsInUseLine: true,
-// 	DisableSuggestions:    true,
-// 	Args:                  cobra.ArbitraryArgs,
-// 	RunE: func(cmd *cobra.Command, args []string) error {
-// 		return cmd.Help()
-// 	},
-// }
-
-// func init() {
-// 	rootCmd.SetHelpTemplate(rootUsageTemplate)
-// }
-
 func main() {
 	ctx := context.TODO()
 
@@ -49,16 +17,6 @@ func main() {
 	// support execution of ai script file (.sh or .yaml)
 	shebang := strings.HasSuffix(args[0], ".yaml") || strings.HasSuffix(args[0], ".sh")
 
-	// // if no args and no input (piped), show help - short form
-	// // $ ai
-	// if len(args) <= 1 && !shebang {
-	// 	if err := rootCmd.Execute(); err != nil {
-	// 		internal.Exit(ctx, err)
-	// 	}
-	// 	return
-	// }
-
-	// shebang support
 	if shebang {
 		args = append([]string{"/sh:bash", "--script", args[0]}, args[1:]...)
 	} else {
@@ -66,23 +24,6 @@ func main() {
 			args = []string{"/help:help"}
 		}
 	}
-
-	// slash commands
-	// intercept builtin commands
-	// $ ai /help [agents|commands|tools|info]
-	//
-	// if strings.HasPrefix(args[1], "/") {
-	// 	switch args[1] {
-	// 	case "/help":
-	// 		err := agent.Help(ctx, args)
-	// 		if err != nil {
-	// 			internal.Exit(ctx, err)
-	// 		}
-	// 		return
-	// 	default:
-
-	// 	}
-	// }
 
 	if err := agent.Run(ctx, args); err != nil {
 		internal.Exit(ctx, err)
