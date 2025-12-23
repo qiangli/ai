e6101fe865342d26e53f688a84904f6972852417
Qiang Li <liqiang@gmail.com>
2025-12-22 16:33:41 -0800

    script all.sh for test

---
+++ test/all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -xue
+
+# this script can be run as system bash
+
+# test/backoff.sh
+# test/chain.sh
+test/eval.sh
+test/script.sh
+
+# TODO fix concurrent writes for parallel
+# test/flow.sh
+
+# test/sb.json.sh
+# test/sb.md
+# test/sb.sh
+# test/sb.txt
+# test/sb.yaml
+# test/timout.sh
+# test/yaml-wrap.sh
+
+echo "Test completed"
--- test/flow.sh
+++ test/flow.sh
@@ -31,21 +31,20 @@ template='data:,
 # $DRY /flow:sequence --actions '["sh:parse", "sh:set_envs", "sh:format"]' --command="$command" --template "$template"
 
 # flow types
-# $DRY /flow:sequence --actions '["/sh:pwd", "fs:list_roots", "agent:ed"]' --option command="ls -al" --option adapter="echo" --option query="what is unix" --option template=$template
+$DRY /flow:sequence --actions '["/sh:pwd", "fs:list_roots", "agent:ed"]' --option command="ls -al" --option adapter="echo" --option query="what is unix" --option template=$template
 
-# $DRY /flow:choice --actions '["/sh:pwd", "fs:list_roots", "agent:ed"]' --option command="ls -al" --option adapter="echo" --option query="what is unix" --option template=$template
+$DRY /flow:choice --actions '["/sh:pwd", "fs:list_roots", "agent:ed"]' --option command="ls -al" --option adapter="echo" --option query="what is unix" --option template=$template
 
 # $DRY /flow:parallel --actions '["/sh:pwd", "fs:list_roots", "agent:ed"]' --option command="ls -al" --option adapter="echo" --option query="what is unix" --option template=$template --option adapter="echo"
-# $DRY /flow:parallel --actions '["/sh:format", "/sh:format", "/sh:format"]' --option query='query x' --template 'data:, *** {{.kit}}:{{.name}} query: {{.query}}' 
+$DRY /flow:parallel --actions '["/sh:format", "/sh:format", "/sh:format"]' --option query='query x' --template 'data:, *** {{.kit}}:{{.name}} query: {{.query}}' 
 
-# $DRY /flow:map --actions '["sh:format"]' --option query='["a", "b", "c"]' --template 'data:, *** {{.kit}}:{{.name}} input: {{.query}}' 
-
+$DRY /flow:map --actions '["sh:format"]' --option query='["a", "b", "c"]' --template 'data:, *** {{.kit}}:{{.name}} input: {{.query}}' 
 
 # $DRY /flow:chain --actions '["sh:pwd", "fs:list_roots", "agent:ed", "sh:format"]'  --option adapter="echo" --option query="what is unix" --option template=$template
 # $DRY /flow:chain --actions '["sh:pwd", "fs:list_roots", "agent:ed", "sh:format"]'  --option adapter="echo" --option query="what is unix" --option template='data:,{{.query}}'
 # $DRY /flow:chain --actions '["sh:parse", "ai:call_llm", "sh:format"]' --option command="/ai:pass --option query='tell me a joke' --option template='data:,>>>this is a test\n {{.result}} error: {{.error}}'" --output console
 
-$DRY /sh:timeout 
+# $DRY /sh:timeout 
 # $DRY /flow:chain --actions '["sh:parse", "ai:call_llm", "sh:format"]' --option command="/ai:pass --option query='tell me a joke' --option template='data:,>>>this is a test\n {{.result}} error: {{.error}}'" --output console
 
 #
