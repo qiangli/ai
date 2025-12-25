f3b7fbbbcab898ab1847963c40eadbeaacbe0efc
Qiang Li <liqiang@gmail.com>
2025-12-25 06:06:45 -0800

    fix memory/memory path

--- test/agent.sh
+++ test/agent.sh
@@ -2,17 +2,32 @@
 set -xue
 
 # agent="${1:-ask}"
-agent="memory"
+# agents="memory/memory" "memory/long_term" "memory/todo_list"
+
+agents=("memory/memory" "memory/long_term" "memory/todo_list")
+
 adapter="echo"
+# template='data:,
+# >>> History
+# {{toPrettyJson .history}}
+
+# >>> Environment
+# {{printenv}}
+# '
 template='data:,
->>> History
-{{toPrettyJson .history}}
+>>> Environment
+{{printenv}}
 '
+
 
 # actions="[ai:call_llm,sh:format]"
 actions="[ai:read_agent_config,ai:new_agent,ai:build_model,ai:build_query,ai:build_prompt,ai:build_context,ai:call_llm,sh:format]"
 
-/flow:sequence  --actions "$actions" --agent "$agent" --adapter "$adapter" --template "$template" $@
+for agent in "${agents[@]}"
+do
+  echo "$agent"
+  /flow:sequence  --actions "$actions" --agent "$agent" --adapter "$adapter" --template "$template" $@
+done
 
 echo "$?"
 echo "*** agent tests completed ***"
