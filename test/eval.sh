7e3eea508236499aba3cad3ec830e3b972fcf830
Qiang Li <liqiang@gmail.com>
2026-01-03 16:28:07 -0800

    minor change for web agent cfg

--- test/eval.sh
+++ test/eval.sh
@@ -17,8 +17,6 @@ template='data:,
 
 >>>>>>>>> Result
 
-{{.}}
-
 >>>>>>>>> Env
 
 {{printenv}}
@@ -26,13 +24,7 @@ template='data:,
 
 ##
 # script="file:///$PWD/swarm/atm/resource/incubator/agents/meta/agent.yaml"
-# script="file:///$PWD/swarm/atm/resource/incubator/agents/gptr/agent.yaml"
-# script="$PWD/swarm/atm/resource/incubator/agents/gptr/agent.yaml"
-# script="file:///$PWD/swarm/atm/resource/incubator/agents/search/agent.yaml"
-# script="file:///$PWD/swarm/atm/resource/incubator/agents/research/agent.yaml"
-# script="file:///$PWD/swarm/atm/resource/incubator/agents/deep/agent.yaml"
-
-script="file:///$PWD/swarm/atm/resource/incubator/agents/test/agent.yaml"
+script="file:///$PWD/swarm/atm/resource/incubator/agents/web/agent.yaml"
 
 # message="write a report on the major world events for the year of 2025"
 # message="What is the latest technology and research  human like consciousness for  AI and LLM"
@@ -42,53 +34,24 @@ script="file:///$PWD/swarm/atm/resource/incubator/agents/test/agent.yaml"
 # message="Plan a cruise trip for my family"
 # message="suggestion for spending a day in Los Angels"
 # message="what is the state of the art for LLM vibe coding and future developments in the next 2 years"
-message="what is the state of the art for LLM memory management and the future research in the next 2 years"
+# message="what is the state of the art for LLM memory management and the future research in the next 2 years"
+message="Top open source github repo for comamnd line parsing in golang"
+
+/agent:web/search \
+    --option message="$message"
 
 # /flow:sequence \
-#     --agent "deep/deep" \
+#     --agent "web/search" \
 #     --actions "[ai:spawn_agent,sh:format]" \
 #     --adapter "echo" \
 #     --template "$template" \
 #     --script "$script" \
 #     --message "$message"
 
-# /agent:deep \
-#     --script "$script" \
-#     --option  query="$message"
-
-# /agent:research \
-#     --script "$script" \
-#     --option  query="$message"
-
-# /agent:research/sub_agent \
-#     --script "$script" \
-#     --option  query="$message"
-
-# /agent:research/web_search \
-#     --script "$script" \
-#     --option  query="$message"
-
-# /research:think_tool \
-#     --script "$script" \
-#     --option  reflection="$message"
-
-# /agent:test/test \
-#     --script "$script" \
-#     --option  query="$message"
-
-/flow:sequence \
-    --agent "test/test" \
-    --actions "[ai:spawn_agent,sh:format]" \
-    --adapter "echo" \
-    --template "$template" \
-    --script "$script" \
-    --message "$message"
-
 ###
-# echo "---- script env ----"
 
 printenv
 
 # echo ""
-# echo "*** eval tests completed ***"
+echo "*** eval tests completed ***"
 ###
