e209b8931d76d761ccb937329b9ff12d92a1078f
Qiang Li <liqiang@gmail.com>
2026-01-18 11:13:43 -0800

    merge env/args and made availble to go as env

--- swarm/lang/yaegi.go
+++ swarm/lang/yaegi.go
@@ -5,15 +5,17 @@ import (
 	"context"
 	"io/fs"
 	"os"
+	"strings"
 
 	"github.com/traefik/yaegi/interp"
 	"github.com/traefik/yaegi/stdlib"
 )
 
 // Golang interepter
-func Golang(ctx context.Context, f fs.FS, env []string, script string, args []string) (any, error) {
+func Golang(ctx context.Context, f fs.FS, global []string, script string, args []string) (any, error) {
 	var b bytes.Buffer
 	goPath := os.Getenv("GOPATH")
+	env := mergetEnvArg(global, args)
 	i := interp.New(interp.Options{
 		Stdout:               &b,
 		Stderr:               &b,
@@ -29,4 +31,32 @@ func Golang(ctx context.Context, f fs.FS, env []string, script string, args []st
 		return nil, err
 	}
 	return b.String(), nil
+}
+
+// merge env and args with args having a higher precedence
+func mergetEnvArg(env []string, args []string) []string {
+	var all = make(map[string]string)
+
+	for _, e := range env {
+		parts := strings.SplitN(e, "=", 2)
+		key := parts[0]
+		if len(parts) == 2 {
+			all[key] = parts[1]
+		}
+	}
+
+	for _, a := range args {
+		parts := strings.SplitN(a, "=", 2)
+		key := parts[0]
+		if len(parts) == 2 {
+			all[key] = parts[1]
+		}
+	}
+
+	var result []string
+	for key, value := range all {
+		result = append(result, key+"="+value)
+	}
+
+	return result
 }
