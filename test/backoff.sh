cf825909e49673bce00ec436c7b9df4b694b466f
Qiang Li <liqiang@gmail.com>
2026-01-19 11:58:52 -0800

    add exampels for sh:backoff tool

--- test/backoff.sh
+++ test/backoff.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env ai /sh:bash --format raw --script
-set -xue
+set -ue
 
 
 echo ">>> Testing backoff..."
 
 # backoff tests
-/sh:backoff --command "/sh:exec --command 'pwd'"  --option duration="10s"
-
-# /sh:exec --command "no_such_cmd"
-# /sh:backoff --command "/sh:exec --command 'no_such_cmd'"  --option duration="10s"
-
-/sh:backoff --command '/flow:choice --actions "[\"sh:pass\",\"invalid_action\",\"kit:invalid_tool\"]"'  --option duration="15s"
-/sh:backoff --option action="/alias:choose" --option choose='/flow:choice --actions "[\"sh:pass\",\"no_such_cmd\",\"sh:pwd\",\"kit:invalid_tool\"]"' --option duration="15s"
+# system command action
+/sh:backoff --command "/sh:exec --command 'printenv'"  --option duration="10s"
+# tool action
+/sh:backoff --command "/fs:list_roots"  --option duration="10s"
+# agent action
+/sh:backoff --command "/agent:ed/ed --adapter echo"  --option duration="10s"
+# flow:choice
+/sh:backoff --command "/flow:choice --actions '[sh:pass,invalid_action,kit:invalid_tool]'"  --duration "15s"
+#
+/sh:backoff --action "/alias:choose" --option choose="/flow:choice --actions '[sh:printenv,no_such_cmd,sh:pwd,kit:invalid_tool]'" --duration "15s"
 
 echo "$?"
 echo "*** 🎉 backoff tests completed ***"
