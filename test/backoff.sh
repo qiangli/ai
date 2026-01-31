3467750c08b7da20a87cdd9f46389b126c93f7d8
Qiang Li <liqiang@gmail.com>
2026-01-30 18:48:29 -0800

    fix tests

--- test/backoff.sh
+++ test/backoff.sh
@@ -8,13 +8,13 @@ echo ">>> Testing backoff..."
 # system command action
 /sh:backoff --command "/sh:exec --command 'printenv'"  --option duration="10s"
 # tool action
-/sh:backoff --command "/fs:list_roots"  --option duration="10s"
+/sh:backoff --command "/sh:pass"  --option duration="10s"
 # agent action
 /sh:backoff --command "/agent:ed/ed --adapter echo --output none"  --option duration="10s"
 # flow:choice
-/sh:backoff --command "/flow:choice --actions '[sh:pass,invalid_action,kit:invalid_tool]'"  --duration "15s"
+/sh:backoff --command "/flow:choice --actions '[sh:pass,invalid_action,kit:invalid_tool]'"  --duration "30s"
 #
-/sh:backoff --action "/alias:choose" --option choose="/flow:choice --actions '[sh:printenv,no_such_cmd,sh:pwd,kit:invalid_tool]'" --duration "15s"
+/sh:backoff --action "/alias:choose" --option choose="/flow:choice --actions '[sh:pass,no_such_cmd,sh:pwd,kit:invalid_tool]'" --duration "30s"
 
 echo "$?"
 echo "*** 🎉 backoff tests completed ***"
